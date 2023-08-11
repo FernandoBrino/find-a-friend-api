f0318ff84d835da7ee7fd401c2282bd9f6b0b862
Fernando Brino <fernandobrino11@gmail.com>
2023-08-11 11:50:25 -0300

    test(unit): should hash org password

--- src/services/create-org.spec.ts
+++ src/services/create-org.spec.ts
@@ -2,6 +2,7 @@ import { InMemoryOrgsRepository } from "@/repositories/in-memory/in-memory-orgs-
 import { describe, beforeEach, it, expect } from "vitest";
 import { CreateOrgService } from "./create-org";
 import { OrgAlreadyExistsError } from "./errors/org-already-exists.error";
+import { compare } from "bcryptjs";
 
 let orgsRepository: InMemoryOrgsRepository;
 let sut: CreateOrgService;
@@ -15,6 +16,7 @@ describe("Create Org Service", () => {
   it("should be able to create", async () => {
     const { org } = await sut.execute({
       name: "Dogs Donation ORG",
+      password: "12345678",
       email: "dogs@example.com",
       phone: "130987651123",
       city: "São Paulo",
@@ -23,11 +25,29 @@ describe("Create Org Service", () => {
     expect(org.id).toEqual(expect.any(String));
   });
 
+  it("should hash user password upon registration", async () => {
+    const { org } = await sut.execute({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password: "123456",
+      phone: "1301111822",
+      city: "Rio De Janeiro",
+    });
+
+    const isPasswordCorrectlyHashed = await compare(
+      "123456",
+      org.password_hash
+    );
+
+    expect(isPasswordCorrectlyHashed).toBe(true);
+  });
+
   it("should not be able to create with same email twice", async () => {
     const email = "dogs@example.com";
 
     await sut.execute({
       name: "Org Example",
+      password: "12345678",
       email,
       phone: "13981190922",
       city: "São Paulo",
@@ -36,6 +56,7 @@ describe("Create Org Service", () => {
     await expect(() =>
       sut.execute({
         name: "Org Example 2",
+        password: "12345678",
         email,
         phone: "13981190922",
         city: "São Paulo",
