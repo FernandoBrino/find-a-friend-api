62a060a69274a1cff5205a54b47d26a1c21f97e6
Fernando Brino <fernandobrino11@gmail.com>
2023-08-10 23:37:07 -0300

    test(unit): should not be able to create org with same email

--- src/services/create-org.spec.ts
+++ src/services/create-org.spec.ts
@@ -1,6 +1,7 @@
 import { InMemoryOrgsRepository } from "@/repositories/in-memory/in-memory-orgs-repository";
 import { describe, beforeEach, it, expect } from "vitest";
 import { CreateOrgService } from "./create-org";
+import { OrgAlreadyExistsError } from "./errors/org-already-exists.error";
 
 let orgsRepository: InMemoryOrgsRepository;
 let sut: CreateOrgService;
@@ -20,5 +21,25 @@ describe("Create Org Service", () => {
     });
 
     expect(org.id).toEqual(expect.any(String));
+  });
+
+  it("should not be able to create with same email twice", async () => {
+    const email = "dogs@example.com";
+
+    await sut.execute({
+      name: "Org Example",
+      email,
+      phone: "13981190922",
+      city: "São Paulo",
+    });
+
+    await expect(() =>
+      sut.execute({
+        name: "Org Example 2",
+        email,
+        phone: "13981190922",
+        city: "São Paulo",
+      })
+    ).rejects.toBeInstanceOf(OrgAlreadyExistsError);
   });
 });
