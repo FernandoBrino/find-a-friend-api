d63152171758a3a14540576e6310b0575d37988d
Fernando Brino <fernandobrino11@gmail.com>
2023-08-11 14:27:47 -0300

    feat: authenticate service

---
+++ src/services/authenticate.ts
@@ -0,0 +1,38 @@
+import { Org } from "@prisma/client";
+import { OrgsRepository } from "@/repositories/orgs-repository";
+import { compare } from "bcryptjs";
+import { InvalidCredentialsError } from "./errors/invalid-credentials.error";
+
+interface AuthenticateServiceRequest {
+  email: string;
+  password: string;
+}
+
+interface AuthenticateServiceResponse {
+  org: Org;
+}
+
+export class AuthenticateService {
+  constructor(private orgsRepository: OrgsRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateServiceRequest): Promise<AuthenticateServiceResponse> {
+    const org = await this.orgsRepository.findByEmail(email);
+
+    if (!org) {
+      throw new InvalidCredentialsError();
+    }
+
+    const doesPasswordMatches = await compare(password, org.password_hash);
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError();
+    }
+
+    return {
+      org,
+    };
+  }
+}
