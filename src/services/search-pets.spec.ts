c2bf07f881d6f78a7451fe0bfe1394ca7870606c
Fernando Brino <fernandobrino11@gmail.com>
2023-08-12 13:23:57 -0300

    test(unit): search pet service

---
+++ src/services/search-pets.spec.ts
@@ -0,0 +1,35 @@
+import { describe, beforeEach, it, expect } from "vitest";
+import { InMemoryPetsRepository } from "@/repositories/in-memory/in-memory-pets-repository";
+import { GetPetById } from "./get-pet-by-id";
+import { SearchPetsService } from "./search-pets";
+
+let petsRepository: InMemoryPetsRepository;
+let sut: SearchPetsService;
+
+describe("Searchs Pets Service", () => {
+  beforeEach(() => {
+    petsRepository = new InMemoryPetsRepository();
+    sut = new SearchPetsService(petsRepository);
+  });
+
+  it("should be able to search for a pet", async () => {
+    await petsRepository.create({
+      name: "Fluffy",
+      description: "Anger",
+      age: "ADULT",
+      size: "LITTLE",
+      org_id: "Org 1",
+    });
+
+    const { pets } = await sut.execute({ query: "ADULT" });
+
+    expect(pets).toEqual([
+      expect.objectContaining({
+        name: "Fluffy",
+        description: "Anger",
+        age: "ADULT",
+        size: "LITTLE",
+      }),
+    ]);
+  });
+});
