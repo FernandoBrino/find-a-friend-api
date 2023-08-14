0e5466ff131bff952fa0fc502228524369ac892a
Fernando Brino <fernandobrino11@gmail.com>
2023-08-14 13:49:42 -0300

    feat: register fastify jwt plugin

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,21 @@
+import fastifyJwt from "@fastify/jwt";
 import fastify from "fastify";
 import { ZodError } from "zod";
+import { env } from "./env";
+import { orgsRoutes } from "./http/controllers/orgs/routes";
+import { petsRoutes } from "./http/controllers/pets/routes";
 
 export const app = fastify();
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  sign: {
+    expiresIn: "10m",
+  },
+});
+
+app.register(orgsRoutes);
+app.register(petsRoutes);
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
